23057be7d4ff4eef7024fc8d4518cb1e426097f3
fightiing <3050023492@qq.com>
2026-08-12 17:22:23 +0800

    Upload Drug_OCR/extractor/__init__.py

---
+++ Drug_OCR/extractor/__init__.py
@@ -0,0 +1,11 @@
+"""药品说明书信息提取模块包。
+
+该包提供:
+- 药品规则提取器
+- 命名实体识别(NER)
+- 知识图谱构建
+- 文本清洗功能
+"""
+
+__version__ = "1.0.0"
+__all__ = ["drug_rule_extractor", "ner_extractor", "kg_builder", "text_cleaner"]
